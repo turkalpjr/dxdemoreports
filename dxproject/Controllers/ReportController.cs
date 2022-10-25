3cb6287a10bf7bd5d58d4431eec10e4c93e8ad7e
Türkalp Kucur <turkalp.kucur@gmail.com>
2022-10-25 17:14:25 +0300

    first commit
    first commit

---
+++ dxproject/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dxproject.Controllers
+{
+  public class ReportController : Controller
+  {
+    public IActionResult Devextreme()
+    {
+      return View();
+    }
+
+
+    public IActionResult FreeReportTools()
+    {
+      return View();
+    }
+
+
+  }
+}
